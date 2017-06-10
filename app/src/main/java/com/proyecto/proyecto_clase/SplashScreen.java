455d1cd7d11ab6a1a1652158f967cd3c335e4fe1
AndroidM97 <monsteryp7@gmail.com>
2017-06-10 16:16:18 +0200

    lim

--- app/src/main/java/com/proyecto/proyecto_clase/SplashScreen.java
+++ app/src/main/java/com/proyecto/proyecto_clase/SplashScreen.java
@@ -4,14 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-/**
- * Created by vamsikrishna on 12-Feb-15.
- */
+
 public class SplashScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
+        //TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
